5f0f2cd4f38ac1f949ac6002a7fe1ea829bfdd50
unknown <marcelobettini@yahoo.com.ar>
2022-04-13 18:20:47 -0300

    isAuth refactor: unnecesary trt/catch

--- Clase09/middlewares/isAuth.js
+++ Clase09/middlewares/isAuth.js
@@ -1,27 +1,18 @@
 const { tokenVerify } = require("../utils/handleJWT");
-
-const isAuth = async (req, res, next) => {
-  try {
+const isAuth = async(req, res, next) => {
     if (!req.headers.authorization) {
-      let error = new Error("No token provided");
-      error.status = 403;
-      return next(error);
+        let error = new Error("No token provided");
+        error.status = 403;
+        return next(error);
     }
     const token = req.headers.authorization.split(" ").pop();
     const isValidToken = await tokenVerify(token);
     if (isValidToken instanceof Error) {
-      console.log("entré en check de validToken");
-      error.message = "Token invalid";
-      error.status = 403;
-      return next(error);
+        let error = new Error("Invalid or Expired Token")
+        error.status = 403;
+        return next(error);
     }
     req.token = isValidToken;
     next();
-  } catch (error) {
-    console.log("salí por el catch");
-    error.message = "Internal Server Error";
-    return next(error);
-  }
 };
-
 module.exports = isAuth;
